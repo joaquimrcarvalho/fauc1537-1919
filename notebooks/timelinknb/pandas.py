5f2913223bd15c0b12e13908b1ea2100739279a7
Joaquim Carvalho <joaquim@uc.pt>
2022-06-01 14:54:36 +0800

    Add name_to_df function

--- notebooks/timelinknb/pandas.py
+++ notebooks/timelinknb/pandas.py
@@ -6,7 +6,8 @@
 #                               column_name='child', # instead of function
 #                               person_info=True, # adds function_name, function_sex, 
 #                                                 #  columns
-#                               filter_by=List,
+#                               dates_in=(after,before),  
+#                               name_like = name of person with function
 #                               more_funcs=['pn','mn','ppn','mpn','pmn','mmn'],....)
 
 from distutils.log import warn
@@ -15,11 +16,52 @@ import pandas as pd
 from matplotlib import cm, colors
 from IPython.display import display
 
+from timelink.mhk.models import base  # noqa
 from timelink.mhk.models.db import TimelinkDB
+from timelink.mhk.models.person import Person
 from timelinknb.config import Session
-from timelinknb import get_attribute_table, get_nattribute_table
+from timelinknb import get_attribute_table, get_nattribute_table, get_person_table
 import timelinknb.config as conf
 
+def remove_particles(name):
+    particles = ("de","da","e","das","dos","do")
+    return " ".join([n for n in name.split() if n not in particles])
+
+def name_to_df(name,db: TimelinkDB=None, similar=False, sql_echo=False ):
+    """ name_to_df return df of people with a matching name
+
+    """
+    # We try to use an existing connection and table introspection
+    # to avoid extra parameters and going to database too much
+    dbsystem: TimelinkDB = None
+    if db is not None:
+        dbsystem = db
+    elif conf.TIMELINK_DBSYSTEM is not None:
+        dbsystem = conf.TIMELINK_DBSYSTEM
+    else:
+        raise(Exception("must call get_mhk_db(conn_string) to set up a database connection before or specify previously openned database with db="))
+
+    if similar:
+        name_particles = remove_particles(name).split(" ")
+        name_like = '%'.join(name_particles)
+        name_like = '%'+name_like+'%'
+    else:
+        name_like = name
+    
+    ptable = get_person_table(dbsystem)
+
+    stmt = select(ptable.c.id, ptable.c.name, ptable.c.sex, ptable.c.obs).where(ptable.c.name.like(name_like))
+    
+    if sql_echo:
+        print(stmt)
+
+    with Session() as session:
+        records = session.execute(stmt)
+        df =  pd.DataFrame.from_records(records.columns('id','name','sex','obs'),index=['id'],columns=['id','name','sex','obs'])
+        return df
+
+
+    
 
 def attribute_to_df(the_type,
                           the_value=None,
